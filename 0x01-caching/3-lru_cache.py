b5366407a1c9360ce92f6c78af3ce8e470f67036
Edward-Odey <edwardodey04@gmail.com>
2024-04-03 09:35:24 +0100

    created LRUCache Class

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""LRUCache Module"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRUCache Class that implement the LRU cache
+    replacement policies"""
+
+    def __init__(self):
+        """LRUCache class initialization method"""
+        super().__init__()
+        self.__usage_tracker = {}
+
+    def __get_key_by_value(self, d, value):
+        """get key from a dict by value"""
+        for key, val in sorted(d.items()):
+            if val == value:
+                return key
+
+    def put(self, key, item):
+        """insert item to the cache storage"""
+        if key and item:
+            if len(self.cache_data.values()) < BaseCaching.MAX_ITEMS:
+                if self.__usage_tracker:
+                    value = max(self.__usage_tracker.values())
+                    self.__usage_tracker[key] = value + 1
+                else:
+                    self.__usage_tracker[key] = 0
+
+                self.cache_data[key] = item
+            else:
+                value = min(self.__usage_tracker.values())
+
+                k = self.__get_key_by_value(self.__usage_tracker, value)
+                del self.cache_data[k]
+                del self.__usage_tracker[k]
+
+                value = max(self.__usage_tracker.values())
+                self.__usage_tracker[key] = value + 1
+                self.cache_data[key] = item
+
+                if key != k:
+                    print('DISCARD: {}'.format(k))
+
+    def get(self, key):
+        """retrive item from the cache storage"""
+        if key in self.cache_data:
+            if key in self.__usage_tracker:
+                value = max(self.__usage_tracker.values())
+                self.__usage_tracker[key] = value + 1
+            return self.cache_data[key]
+        return None
