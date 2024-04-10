bdcce7920fce1229e7da2314fde13924cf3d9ea5
Edward-Odey <edwardodey04@gmail.com>
2024-04-10 14:47:16 +0100

    create 5-app.py

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""Flask App Module"""
+
+from flask import Flask, render_template, request
+from flask import g
+from flask_babel import Babel
+
+
+class Config(object):
+    """configuration class for flask_babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """retrive a user data"""
+    user_id = request.args.get('login_as')
+    if user_id is not None:
+        user = users.get(int(user_id))
+        if user is not None:
+            return user
+    return None
+
+
+@app.before_request
+def before_request():
+    """method execute before every request"""
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale():
+    """determine the best match with our supported languages"""
+    if 'locale' in request.args:
+        locale = request.args['locale']
+        if locale in Config.LANGUAGES:
+            return locale
+
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def index():
+    """render index.html file"""
+
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
