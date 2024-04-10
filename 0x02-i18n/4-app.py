e043503405f9f078f176998851608e8ab3e13e60
Edward-Odey <edwardodey04@gmail.com>
2024-04-10 13:48:45 +0100

    added logic for url query parameter

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Flask App Module"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """configuration class for flask_babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """determine the best match with our supported languages"""
+    if 'locale' in requst.args:
+        return request.args['locale']
+
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def index():
+    """render index.html file"""
+
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
